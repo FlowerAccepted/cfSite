10df93d4a687665a08a2bde4eb35a1f4cc016738
FlowerAccpeted <accepted_flower@outlook.com>
2026-01-23 20:50:01 +0800

    Improved the visual effects on the frontend

--- public/index.js
+++ public/index.js
@@ -1,26 +1,7 @@
-// 简单的 Markdown 渲染器
-function renderMarkdown(text) {
-    // 简单的替换，实际可以使用库如 marked
-    return text
-        .replace(/^# (.*$)/gim, '<h1>$1</h1>')
-        .replace(/^## (.*$)/gim, '<h2>$1</h2>')
-        .replace(/^### (.*$)/gim, '<h3>$1</h3>')
-        .replace(/^\* (.*$)/gim, '<li>$1</li>')
-        .replace(/^- (.*$)/gim, '<li>$1</li>')
-        .replace(/\*\*(.*)\*\*/gim, '<strong>$1</strong>')
-        .replace(/\*(.*)\*/gim, '<em>$1</em>')
-        .replace(/\n\n/gim, '</p><p>')
-        .replace(/\n/gim, '<br>')
-        .replace(/(<li>.*<\/li>)/g, '<ul>$1</ul>'); // 简单包装列表
-}
-
 // 加载数据
 fetch('data.json')
     .then(response => response.json())
     .then(data => {
-        // 渲染 Markdown
-        document.getElementById('markdown-render').innerHTML = '<p>' + renderMarkdown(data.markdown) + '</p>';
-
         // 渲染类别
         const categoriesDiv = document.getElementById('categories');
         data.categories.forEach(category => {
---
+++ src/markdown.md
@@ -0,0 +1,11 @@
+<center><img alt="" src="favicon.svg" width="320" height="320"></center>
+
+# 欢迎来到 FlowerAccepted 的网站！
+
+发现实用工具，阅读绝妙文章，探索新鲜知识。欢迎光临拙站！
+
+<p style="color:#FFC116">（本站目前仍在大力建设中，敬请期待更多内容喵~）</p>
+
+[本站开源仓库](https://github.com/FlowerAccepted/cfSite)，欢迎贡献！
+
+### <p style="color:#FF0000">注意：用户系统正在热开发，欢迎测试，请勿正式使用！否则出现后续账号被删的后果自负！</p>
